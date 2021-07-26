eb07da55b208e30ada94410062c1169f5e655062
Aoibhinn <97aoibhinn@gmail.com>
2021-07-26 09:24:13 +0000

    Add new_game function to set board size, number of ships, welcome message and prompt player for their name

--- run.py
+++ run.py
@@ -1,1 +1,42 @@
-# python code goes here
+from random import randint
+
+scores = {"computer": 0, "player": 0}
+
+
+class Board:
+    """
+    Main board class, Sets board size, the number of ships,
+    the player's name and the board type (player board or computer)
+    Has methods for adding ships and guesses and printing the board
+    """
+
+    def ___init___(self, size, num_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+
+def new_game():
+    """
+    Starts a new game. Sets the board size and number of ships, resets the
+    scores and initialises the boards.
+    """
+
+    size = 5
+    num_ships = 4
+    scores["computer"] = 0
+    scores["player"] = 0
+    print("-" * 35)
+    print("Welcome to ULTIMATE BATTLESHIPS!!")
+    print(f"Board Size: {size}. Number of ships: {num_ships}")
+    print(" Top left corner is row: 0, col: 0")
+    print("-" * 35)
+    player_name = input("Please enter your name: \n")
+    print("-" * 35)
+
+
+new_game()
